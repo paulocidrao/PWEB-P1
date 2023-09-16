b6c36e7e5906eb8b432a6de2c99873b1e472dc32
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2023-09-16 20:17:29 -0300

    Pagina inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Home } from "./pages/Home"
+
+function App() {
+  return (
+    <main className="bg-gray-600 w-screen h-screen">
+      <Home/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/utils/Buttons/Comprar/Comprar.tsx
@@ -0,0 +1,5 @@
+export const Comprar = () =>{
+    return(
+        <button className="bg-blue-500 text-white font-bold p-2 rounded w-full hover:bg-blue-700" >Comprar</button>
+    )
+}
